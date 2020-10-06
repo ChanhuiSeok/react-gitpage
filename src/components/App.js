57e99520010b3e50cd9b99111932f442f2d36146
unknown <chanhuicom@gmail.com>
2020-10-06 23:42:18 +0900

    프로젝트 디렉토리 구조 변경

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {HashRouter as Router, Route} from "react-router-dom"
+import Home from "../routes/Home"
+import Detail from "../routes/Detail"
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    word-spacing:-1px;
+  }
+`;
+
+function App() {
+  return (
+      <>
+       <GlobalStyles/>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/detail" component={Detail} />
+      </Router>
+      </>
+  );
+}
+export default App;
