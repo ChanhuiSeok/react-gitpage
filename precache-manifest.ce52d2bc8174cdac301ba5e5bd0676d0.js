7373f93cd8bb386e984df904d748bfab03b65f18
unknown <chanhuicom@gmail.com>
2020-10-19 12:19:28 +0900

    Updates

---
+++ precache-manifest.ce52d2bc8174cdac301ba5e5bd0676d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0a6bf00ed57908ea55fa024880db8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d504f8a9a5f5fa2f6d14",
+    "url": "/static/css/main.13cf1909.chunk.css"
+  },
+  {
+    "revision": "18f99dd745c87fe17451",
+    "url": "/static/js/2.12e0b6b3.chunk.js"
+  },
+  {
+    "revision": "a23aa5ad74bbccef876e3906f8c6f1a1",
+    "url": "/static/js/2.12e0b6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d504f8a9a5f5fa2f6d14",
+    "url": "/static/js/main.ded31254.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
